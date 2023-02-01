645f8f6d4087b7854dc9150afb60db67c6af47ed
Armin <123939474+ss-armin@users.noreply.github.com>
2023-02-01 22:03:30 +0330

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'Ss Armin'
+description 'The driver cannot use a gun while driving'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
